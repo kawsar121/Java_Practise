0109fece949eb09381dadb0aa596f61e04ffb893
kawsar121 <tohidulislamkawsarbhuiyan2567gmail.com>
2023-11-30 22:11:50 +0600

    first commit

---
+++ src/First.java
@@ -0,0 +1,136 @@
+                            // ⭐Practise:1
+/* 
+
+public class First {
+
+    public static void main(String[] args) {
+        System.out.print("Tohidul Islam Kawsar Bhuiyan");
+        System.out.println("Roll:683480");
+        System.out.println("Institue:FCI");
+        System.out.println("Regestration: 683480");
+    }
+}
+ */
+
+
+
+
+
+                                    // ⭐Practise:1-alternative(simple:hahaaa) \n mane new line create kora 
+/*   
+
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("Tohidul Islam \n Roll:683480 \n Institue:FCI \n Regestration: 683480 ");
+    }
+}
+ 
+*/
+
+            // ⭐practise 2 , \t mane holo 2 ta jinish likhle potom ta dekhanor por ditiyo ta dekhabe
+/*
+ public class First {
+
+    public static void main(String[] args) {
+        System.out.println("1 \t 2");
+        System.out.println("3 \t 4");
+
+    }
+}
+
+
+*/
+
+            // ⭐practise 3 shortcut (psvm)
+
+/*
+ public class First {
+
+    public static void main(String[] args) { 
+        int a;
+        int b; 
+        a = 2;
+        b= 3;
+        int addition= a+b;
+        System.out.println(addition);
+    } 
+}
+ */
+
+            // ⭐practise 4
+
+/*
+ import java.util.Scanner;
+
+public class First {
+
+    public static void main(String[] args) {
+        Scanner my = new Scanner(System.in) ;
+        int number;
+        System.out.println("Enter type your number");
+        number = my.nextInt();
+        System.out.println("The number is : " + number);
+    }
+}
+ */
+
+
+            // ⭐practise 4 er shortcut with addition
+/* 
+import java.util.Scanner;
+
+
+ public class First {
+
+    public static void main(String[] args) {
+        Scanner my = new Scanner(System.in);
+        System.out.println("The number 1 is : " );
+        int number1 = my.nextInt();
+        System.out.println("The number 2 is : " );
+        int number2 = my.nextInt();
+        int addition = number1+number2;
+        System.out.println("The addition is : "+ addition);
+    }
+}
+ */
+
+                    // ⭐practise 5 Subtraction
+
+/*
+    import java.util.Scanner;
+
+public class First {
+ 
+        public static void main(String[] args) {
+            Scanner you = new Scanner(System.in);
+            System.out.print("Enter the first number : ");
+            int number1 = you.nextInt();
+            System.out.print("Enter the second number : ");
+            int number2 = you.nextInt();
+            int subtruction = number1-number2;
+            System.out.println("The subtruction is : "+subtruction);            
+        }
+ }
+ */
+
+                // ⭐practise 5 multification
+
+import java.util.Scanner;
+
+public class First {
+
+    public static void main(String[] args) {
+        Scanner pop = new Scanner(System.in);
+        System.out.print("First number is : ");
+        int number1 = pop.nextInt(); 
+        System.out.print("Second number is : ");
+        int number2 = pop.nextInt();
+        int multification = number1*number2;
+        System.out.println(multification);
+    }
+}
+
+// Jodi intijer na niya string ni tahle nextInt er jawgaw voshve nextline()
+// Jodi intijer na niya DOuble ni tahle nextInt er jawgaw voshve nextDouble()
+// Jodi intijer na niya Float ni tahle nextInt er jawgaw voshve nextFloat()
