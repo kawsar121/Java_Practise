c2420c7d41f5e24386c98a5764f5e31156868375
kawsar121 <tohidulislamkawsarbhuiyan2567gmail.com>
2023-12-03 23:14:34 +0600

    readme file make concept clear

---
+++ src/whileLoop.java
@@ -0,0 +1,14 @@
+            //practise 1
+/**
+ * whileLoop
+ */
+public class whileLoop {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=10){
+            System.out.println(i);   
+            i++;
+        }
+    }
+}
