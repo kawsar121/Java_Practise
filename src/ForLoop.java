8fdc0ad4809eccb3db464045e6b39e615a4861fe
kawsar121 <tohidulislamkawsarbhuiyan2567gmail.com>
2023-12-03 23:02:01 +0600

    for loop function concept clear

---
+++ src/ForLoop.java
@@ -0,0 +1,22 @@
+                // practise 1
+/*
+ public class ForLoop {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            System.out.println("Bangladesh");
+        }
+    }
+}
+ */
+
+            //practise 2
+
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i=i+2 ){
+            System.out.println(i);
+        }
+    }
+}
