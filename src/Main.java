6dd0f525bbd201545b2881bf98b240596fc6215f
kawsar121 <tohidulislamkawsarbhuiyan2567gmail.com>
2024-01-09 10:38:29 +0600

    all are complete

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Type your first number : ");
+        int a = input.nextInt();
+        System.out.print("Enter Type your first number : ");
+        int b = input.nextInt();
+        int c = a+b;
+        System.out.println(c);
+    }
+}
